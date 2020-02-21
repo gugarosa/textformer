2e3a2c8a26380e5770af336bc99e4656a2eca3b9
Gustavo Rosa <gth.rosa@uol.com.br>
2020-02-21 09:57:59 -0300

    Adding initial files.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='textformer',
+      version='1.0.0',
+      description='Transformer-based Text Operators',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Gustavo Rosa',
+      author_email='gustavo.rosa@unesp.br',
+      url='https://github.com/gugarosa/textformer',
+      license='GPL-3.0',
+      install_requires=['coverage>=5.0.3',
+                        'pylint>=2.4.4',
+                        'pytest>=5.3.5'
+                        ],
+      extras_require={
+          'tests': ['coverage',
+                    'pytest',
+                    'pytest-pep8',
+                    ],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
