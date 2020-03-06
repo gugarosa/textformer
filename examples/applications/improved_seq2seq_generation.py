f61684cd8205e2cd64a4dadd45cdd5db5832bfd9
Gustavo Rosa <gth.rosa@uol.com.br>
2020-03-06 10:59:50 -0300

    Adding an improved Seq2Seq model.

---
+++ examples/applications/improved_seq2seq_generation.py
@@ -0,0 +1,35 @@
+from textformer.datasets.generative import GenerativeDataset
+from textformer.models.improved_seq2seq import Decoder, Encoder, ImprovedSeq2Seq
+from torchtext.data import BPTTIterator, Field
+
+# Defines the device which should be used, e.g., `cpu` or `cuda`
+device = 'cpu'
+
+# Defines the input file
+file_path = 'data/generative/chapter1_harry.txt'
+
+# Defines a datatype for further tensor conversion
+source = Field(batch_first=True, lower=True)
+
+# Creates the GenerativeDataset
+dataset = GenerativeDataset(file_path, source)
+
+# Builds the vocabulary
+source.build_vocab(dataset, min_freq=1)
+
+# Creates an iterator that backpropagates through time
+train_iterator = BPTTIterator(
+    dataset, batch_size=16, bptt_len=10, device=device)
+
+# Creating the Encoder
+encoder = Encoder(n_input=len(source.vocab), n_hidden=512, n_embedding=256)
+
+# Creating the Decoder
+decoder = Decoder(n_output=len(source.vocab), n_hidden=512, n_embedding=256)
+
+# Creating the ImprovedSeq2Seq model
+improved_seq2seq = ImprovedSeq2Seq(encoder, decoder, init_weights=None,
+                                   ignore_token=None, device=device)
+
+# Training the model
+improved_seq2seq.fit(train_iterator, epochs=10)
---
+++ examples/applications/improved_seq2seq_translating.py
@@ -0,0 +1,44 @@
+from textformer.datasets.translation import TranslationDataset
+from textformer.models.improved_seq2seq import Decoder, Encoder, ImprovedSeq2Seq
+from torchtext.data import BucketIterator, Field
+
+# Defines the device which should be used, e.g., `cpu` or `cuda`
+device = 'cpu'
+
+# Defines the input file
+file_path = 'data/translation/europarl'
+
+# Defines datatypes for further tensor conversion
+source = Field(init_token='<sos>', eos_token='<eos>', lower=True)
+target = Field(init_token='<sos>', eos_token='<eos>', lower=True)
+
+# Creates the TranslationDataset
+train_dataset, val_dataset, test_dataset = TranslationDataset.splits(
+    file_path, ('.en', '.pt'), (source, target))
+
+# Builds the vocabularies
+source.build_vocab(train_dataset, min_freq=1)
+target.build_vocab(train_dataset, min_freq=1)
+
+# Gathering the <pad> token index for further ignoring
+target_pad_index = target.vocab.stoi[target.pad_token]
+
+# Creates a bucket iterator
+train_iterator, val_iterator, test_iterator = BucketIterator.splits(
+    (train_dataset, val_dataset, test_dataset), batch_size=2, sort=False, device=device)
+
+# Creating the Encoder
+encoder = Encoder(n_input=len(source.vocab), n_hidden=512, n_embedding=256)
+
+# Creating the Decoder
+decoder = Decoder(n_output=len(target.vocab), n_hidden=512, n_embedding=256)
+
+# Creating the ImprovedSeq2Seq model
+improved_seq2seq = ImprovedSeq2Seq(encoder, decoder, init_weights=None,
+                  ignore_token=target_pad_index, device=device)
+
+# Training the model
+improved_seq2seq.fit(train_iterator, val_iterator, epochs=10)
+
+# Evaluating the model
+improved_seq2seq.evaluate(test_iterator)
