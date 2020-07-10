11bda9cc6b40ec0f76b1a074ffcf6d9410ad6c16
Gustavo Rosa <gth.rosa@uol.com.br>
2020-07-10 17:08:28 -0300

    Bumping version for future release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='textformer',
-      version='1.0.0',
+      version='1.0.1',
       description='Transformer-based Text Operators',
       long_description=long_description,
       long_description_content_type='text/markdown',
